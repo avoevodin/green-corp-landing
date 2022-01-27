fe7ab8a9e6a6e886995c07673ddf5a8481629d4b
avo888 <avoevodin8888@gmail.com>
2022-01-27 03:17:55 +0300

    added bubble class constructor

--- js/canvas.js
+++ js/canvas.js
@@ -1,1 +1,20 @@
-console.log("Hi!")
+class Bubble {
+    constructor(canvas) {
+        this.canvas = canvas
+
+        this.getCanvasSize()
+        this.init()
+    }
+
+    getCanvasSize() {
+
+    }
+
+    init() {
+
+    }
+
+    move() {
+
+    }
+}
