fb67f5a09b7b8af6fbf3c67a738942f13e0031e8
avo888 <avoevodin8888@gmail.com>
2022-01-27 03:37:16 +0300

    added move method for bubble class

--- js/canvas.js
+++ js/canvas.js
@@ -5,29 +5,36 @@ function generateDecimalBetween(left, right) {
 }
 class Bubble {
     constructor(canvas) {
-        this.canvas = canvas
+        this.canvas = canvas;
 
-        this.getCanvasSize()
-        this.init()
+        this.getCanvasSize();
+        this.init();
     }
 
     getCanvasSize() {
-        this.canvasWidth = this.canvas.clientWidth
-        this.canvasHeigth = this.canvas.clientHeight
+        this.canvasWidth = this.canvas.clientWidth;
+        this.canvasHeigth = this.canvas.clientHeight;
     }
 
     init() {
-        this.color = COLORS[Math.floor(Math.random() * COLORS.length)]
-        this.size = generateDecimalBetween(1, 3)
-        this.alpha = generateDecimalBetween(5, 10) / 10
-        this.translateX = generateDecimalBetween(0, this.canvasWidth)
-        this.translateY = generateDecimalBetween(0, this.canvasHeigth)
-        this.velocity = generateDecimalBetween(20, 40)
-        this.movementX = generateDecimalBetween(-2, 2) / this.velocity
-        this.movementY = generateDecimalBetween(1, 20) / this.velocity
+        this.color = COLORS[Math.floor(Math.random() * COLORS.length)];
+        this.size = generateDecimalBetween(1, 3);
+        this.alpha = generateDecimalBetween(5, 10) / 10;
+        this.translateX = generateDecimalBetween(0, this.canvasWidth);
+        this.translateY = generateDecimalBetween(0, this.canvasHeigth);
+        this.velocity = generateDecimalBetween(20, 40);
+        this.movementX = generateDecimalBetween(-2, 2) / this.velocity;
+        this.movementY = generateDecimalBetween(1, 20) / this.velocity;
     }
 
     move() {
+        this.translateX = this.translateX - this.movementX;
+        this.translateY = this.translateY - this.movementY;
 
+        if (this.translateX < 0 || this.translateY < 0 ||
+            this.translateX > this.canvasWidth) {
+                this.init();
+                this.translateY = this.canvasHeigth;
+            }
     }
 }
